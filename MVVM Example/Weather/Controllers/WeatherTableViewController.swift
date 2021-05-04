cc5715d3b28d640abcef82c54d0ad0ad7a627f48
Fomagran <fomagran6@naver.com>
2021-05-04 12:00:37 +0900

    Weather 스토리보드 기본 세팅

---
+++ MVVM Example/Weather/Controllers/WeatherTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WeatherTableViewController.swift
+//  MVVM Example
+//
+//  Created by Fomagran on 2021/05/04.
+//
+
+import UIKit
+
+class WeatherTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       
+        return 0
+    }
+
+}
