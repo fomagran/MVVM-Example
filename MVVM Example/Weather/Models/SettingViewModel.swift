d2c983f9fc6fc5c256b29a7ee0c3791649cc3917
Fomagran <fomagran6@naver.com>
2021-05-26 12:03:57 +0900

    SettingViewModel 만들기 ( 섭씨,화씨 표현하기 위한 유닛들 만듦)

---
+++ MVVM Example/Weather/Models/SettingViewModel.swift
@@ -0,0 +1,48 @@
+//
+//  SettingViewModel.swift
+//  MVVM Example
+//
+//  Created by Fomagran on 2021/05/26.
+//
+
+import Foundation
+
+enum Unit:String,CaseIterable {
+    case celsius = "metric"
+    case fahrenheit = "imperial"
+}
+
+extension Unit {
+    var displayName:String {
+        get {
+            switch self {
+            case .celsius:
+                return "Celcius"
+            default:
+                return "Fahrenheit"
+            }
+        }
+    }
+}
+
+class SettingViewModel {
+    let units = Unit.allCases
+    
+    var selectedUnit:Unit {
+        get {
+            let userDefault = UserDefaults.standard
+            var unitValue = ""
+            if let value =  userDefault.value(forKey: "units") as? String {
+                unitValue = value
+            }
+            
+            return Unit(rawValue: unitValue)!
+        }
+        set {
+            let userDefault = UserDefaults.standard
+            userDefault.setValue(newValue.rawValue, forKey: "unit")
+        }
+    }
+}
+
+
